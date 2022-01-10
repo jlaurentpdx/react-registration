63161d52c081641cac172f245087d64219016ae5
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-10 12:55:48 -0800

    form inputs are rendering

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+import './Auth.css';
+
 export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
       <h1>This is the authorization component.</h1>
+      <form className="auth-form">
+        <label className="form-control">
+          email:
+          <input
+            value={email}
+            type="email"
+            placeholder="email@example.com"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className="form-control">
+          password:
+          <input
+            value={password}
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </form>
     </div>
   );
 }
