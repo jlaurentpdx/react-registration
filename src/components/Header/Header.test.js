52df381ede9214724099a7d4b2824f9c29bc8778
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-11 00:16:19 -0800

    added snapshot test for Auth when user is not logged in

--- src/components/Header/Header.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
-
-test('Header should display Login and Register when component mounts', () => {
-  render(<Header />);
-  screen.getByText('Login');
-  screen.getByText('Register');
-});
