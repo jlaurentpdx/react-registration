f98a3aa7d6a323ad08d294564be10cc40391ba49
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-10 17:58:33 -0800

    signInUser functioning correctly by type on page

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -3,40 +3,52 @@ import * as users from '../../services/users';
 import './Auth.css';
 
 export default function Auth() {
+  const [type, setType] = useState('login');
+  const [currentUser, setCurrentUser] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = async (e) => {
+    let response;
     try {
       e.preventDefault();
-      users.signUpUser(email, password);
+      response =
+        type === 'login' ? users.signInUser(email, password) : users.signUpUser(email, password);
     } catch {
       alert('something went wrong');
     }
+    setCurrentUser(response);
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form className="auth-form">
-        <label htmlFor="email">E-mail:</label>
-        <input
-          name="email"
-          value={email}
-          type="email"
-          placeholder="email@example.com"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          name="password"
-          value={password}
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleSubmit}>Submit</button>
-      </form>
-    </div>
+    <>
+      {currentUser ? (
+        <h1>Success</h1>
+      ) : (
+        <div>
+          <h1>Login</h1>
+          <form className="auth-form">
+            <label htmlFor="email">E-mail:</label>
+            <input
+              name="email"
+              value={email}
+              type="email"
+              placeholder="email@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              name="password"
+              value={password}
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+      )}
+      ;
+    </>
   );
 }
