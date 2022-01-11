40883b7a42552a70f85262395202a7c66f62eb22
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-10 17:34:21 -0800

    successfully registering new users

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,32 +1,41 @@
 import { useState } from 'react';
+import * as users from '../../services/users';
 import './Auth.css';
 
 export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      users.signUpUser(email, password);
+    } catch {
+      alert('something went wrong');
+    }
+  };
+
   return (
     <div>
-      <h1>This is the authorization component.</h1>
+      <h1>Register</h1>
       <form className="auth-form">
-        <label className="form-control">
-          email:
-          <input
-            value={email}
-            type="email"
-            placeholder="email@example.com"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label className="form-control">
-          password:
-          <input
-            value={password}
-            type="password"
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
+        <label htmlFor="email">E-mail:</label>
+        <input
+          name="email"
+          value={email}
+          type="email"
+          placeholder="email@example.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          name="password"
+          value={password}
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
