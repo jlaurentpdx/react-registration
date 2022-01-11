9819b00f3f4c50a644f897159677933d67c2c643
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-11 00:05:25 -0800

    added logout and user GET to home page

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import * as users from '../../services/users';
+import Header from '../../components/Header/Header';
 import AuthForm from '../../components/AuthForm/AuthForm';
 import './Auth.css';
-import Header from '../../components/Header/Header';
 
-export default function Auth() {
+export default function Auth({ setCurrentUser }) {
   const [type, setType] = useState('login');
-  const [currentUser, setCurrentUser] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
@@ -27,26 +26,10 @@ export default function Auth() {
 
   return (
     <>
-      {currentUser ? (
-        <>
-          <h1>Welcome.</h1>
-          <h2>Now... chill.</h2>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/5qap5aO4i9A"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </>
-      ) : (
-        <div>
-          <Header {...{ type, setType, setMessage }} />
-          <AuthForm {...{ email, setEmail, password, setPassword, message, handleSubmit }} />
-        </div>
-      )}
+      <div>
+        <Header {...{ type, setType, setMessage }} />
+        <AuthForm {...{ email, setEmail, password, setPassword, message, handleSubmit }} />
+      </div>
     </>
   );
 }
